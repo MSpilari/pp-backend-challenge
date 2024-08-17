42f540b647adb78a66b829a8cff8a6fd43ae60cd
MSpilari <mb.spilari@hotmail.com>
2024-08-17 13:48:21 -0300

    feat: create ResponseStatusException method handler

--- src/main/java/example/com/challengePicPay/configs/GlobalException.java
+++ src/main/java/example/com/challengePicPay/configs/GlobalException.java
@@ -16,4 +16,10 @@ public class GlobalException {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Map.of("error_message", error.getMessage()));
     }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    ResponseEntity<Map<String, String>> responseStatusException(ResponseStatusException error) {
+        return ResponseEntity.status(error.getStatusCode())
+                .body(Map.of("error_message", error.getReason()));
+    }
 }
