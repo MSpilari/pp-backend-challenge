5d1346fe9251c3fcfdf5ca38bee34e3ffa9fcd07
MSpilari <mb.spilari@hotmail.com>
2024-08-19 16:51:46 -0300

    feat(ShopkeeperRepository): create repository

---
+++ src/main/java/example/com/challengePicPay/repositories/ShopkeeperRepository.java
@@ -0,0 +1,12 @@
+package example.com.challengePicPay.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import example.com.challengePicPay.entities.ShopkeeperEntity;
+
+@Repository
+public interface ShopkeeperRepository extends JpaRepository<ShopkeeperEntity, Long> {
+}
