dd251263d46e1f92ca02da5fe7d053d7ed6ab2cc
MSpilari <mb.spilari@hotmail.com>
2024-08-19 16:31:21 -0300

    refactor: passwordEncoder has its own file

---
+++ src/main/java/example/com/challengePicPay/configs/PasswordEncoderConfig.java
@@ -0,0 +1,13 @@
+package example.com.challengePicPay.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+    @Bean
+    BCryptPasswordEncoder bPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
